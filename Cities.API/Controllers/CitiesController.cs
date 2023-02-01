07114b30e121306dfc5543e2344cd0782a554c60
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-01 16:43:57 -0500

    Created first API action

---
+++ Cities.API/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cities.API.Controllers
+{
+    // Attribute that "improves development experience" when building API
+    // E.g. automatically returning 400 bad request on bad input
+    [ApiController]
+    // ControllerBase = contains helper methods in setting up controller
+    public class CitiesController : ControllerBase
+    {
+
+        [HttpGet("api/cities")]
+        // Want to return everything in JSON format 
+        public JsonResult GetCities()
+        {
+            // create constructor that returns JSON-ified data
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, Name = "New York City"},
+                    new { id = 2, Name = "Antwerp"}
+                });
+
+        }
+     
+    }
+}
--- Cities.API/Program.cs
+++ Cities.API/Program.cs
@@ -1,13 +1,17 @@
 namespace Cities.API
 {
+    // Program = starting point of application
     public class Program
     {
+        // Main = configures + runs application
         public static void Main(string[] args)
         {
+            // ** 1. Web application = built (using WebApplicationBuilder)
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            // Add services to the container ("built-in dependency injection container")
+            // I.e. inject them wherever needed in our code
+            // Adding view as well = not necessary for APIs (the view = in JSON format)
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -15,19 +19,35 @@ namespace Cities.API
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // ** 2. Configure request pipeline (via adding "middleware")
+            // Middleware = Configures the HTTP request pipeline
+            // Runs if our environmental variable = set to "development" 
             if (app.Environment.IsDevelopment())
             {
+                // These are development middlewares
+                // Middlewares = package of code that runs between request <-> response
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
 
+            // Setting up request pipeline:
+
+            // ** 1. Install routing middleware
+            app.UseRouting();
+
             app.UseAuthorization();
 
 
-            app.MapControllers();
+            // ** 2. Create an endpoint
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Configures endpoints without specifying routes (specify routes = via attributes)
+                endpoints.MapControllers();
+            });
+             
 
             app.Run();
         }
