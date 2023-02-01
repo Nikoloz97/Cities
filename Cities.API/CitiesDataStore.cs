3b7204e8ef81f0281cfd77e1c85a245d02a72e3e
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-01 17:46:30 -0500

    Created datastore and city model

---
+++ Cities.API/CitiesDataStore.cs
@@ -0,0 +1,43 @@
+﻿using Cities.API.Models;
+
+namespace Cities.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }   
+
+        // "Current" property = returns instance of the CitiesDataStore
+        // Static, so CitiesDataStore doesn't need to be instantiated to access this property
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        // Constructor = contains "dummy data"
+        public CitiesDataStore() 
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with the big park."
+                },
+
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Berlin",
+                    Description = "The capital of the axis in WW2."
+                },
+
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Tokyo",
+                    Description = "The asian capital of the axis."
+                }
+
+            };
+        }
+
+    }
+}
--- Cities.API/Controllers/CitiesController.cs
+++ Cities.API/Controllers/CitiesController.cs
@@ -5,23 +5,23 @@ namespace Cities.API.Controllers
     // Attribute that "improves development experience" when building API
     // E.g. automatically returning 400 bad request on bad input
     [ApiController]
+    // Creates a base path for all following HTTP actions
+    // [controller] = matches the class name's prefix (i.e. api/cities) 
+    [Route("api/[controller]")]
     // ControllerBase = contains helper methods in setting up controller
     public class CitiesController : ControllerBase
     {
-
-        [HttpGet("api/cities")]
+        // URI ends with "api/cities" since that was placed in Route param
+        [HttpGet]
         // Want to return everything in JSON format 
         public JsonResult GetCities()
         {
             // create constructor that returns JSON-ified data
-            return new JsonResult(
-                new List<object>
-                {
-                    new { id = 1, Name = "New York City"},
-                    new { id = 2, Name = "Antwerp"}
-                });
-
+            return new JsonResult(CitiesDataStore.Current.Cities);
+             
         }
+
+
      
     }
 }
