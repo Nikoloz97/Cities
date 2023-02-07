165f33652b02d3431807b0a32a4a8b001ef23e55
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-07 17:41:38 -0500

    Set configuration for mail service class attributes

--- Cities.API/Services/LocalMailService.cs
+++ Cities.API/Services/LocalMailService.cs
@@ -2,10 +2,18 @@
 {
     public class LocalMailService : IMailService
     {
-        // Dummy property information
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
 
+        private readonly string _mailTo = string.Empty;
+        private readonly string _mailFrom = string.Empty;
+
+        // Assign configuration (see appsettings.json)
+        public LocalMailService(IConfiguration configuration)
+        {
+            // Pass through the key of JSON object
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+
+        }
 
         public void Send(string subject, string message)
         {
