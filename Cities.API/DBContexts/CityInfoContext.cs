49ed5b80ae0e028e163730967f5210e7073dbfdc
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-09 21:04:04 -0500

    Created a migration

--- Cities.API/DBContexts/CityInfoContext.cs
+++ Cities.API/DBContexts/CityInfoContext.cs
@@ -1,6 +1,23 @@
-﻿namespace Cities.API.DBContexts
+﻿using Cities.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cities.API.DBContexts
 {
-    public class CityInfoContext
+    // DbContext = class inherited from Entity Framework
+    public class CityInfoContext : DbContext
     {
+        // Dbset = creates instances of its entity type (translated to queries in DB) 
+        public DbSet<City> Cities { get; set; } = null!;
+
+        // DbContext doesn't allow for nulls, so include "null forgiver" (null!)
+        public DbSet<PointOfInterest> PointOfInterest { get; set; } = null!;
+
+        // Constructor purpose = provide options at moment DBContext is registered 
+        // See Services.AddDbContext (program.cs) 
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        { 
+
+        }
+        
     }
 }
---
+++ Cities.API/Migrations/20230210015640_CityInfoDBInitialMigration.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+// Consists of initial migration 
+
+namespace Cities.API.Migrations
+{
+    public partial class CityInfoDBInitialMigration : Migration
+    {
+        // Up = current -> new version
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PointOfInterest",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    CityId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PointOfInterest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PointOfInterest_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PointOfInterest_CityId",
+                table: "PointOfInterest",
+                column: "CityId");
+        }
+
+        // Down = current -> older version
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PointOfInterest");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
