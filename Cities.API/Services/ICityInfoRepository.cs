5951ec3a8b80f28fbe2a0dc0ab8c11372af5ca81
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-11 19:55:40 -0500

    Created cityInfo repository pattern

---
+++ Cities.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Cities.API.Entities;
+
+namespace Cities.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        // Async function = allows for app scalability
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<City?> GetCityAsync(int cityId);
+        Task<IEnumerable<PointofInterest>> GetPointsOfInterestForCityAsync(int cityId);
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
+
+    }
+}
